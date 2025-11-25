13ca58acf4a33dfdf593f87950ce2a21f134a809
hwhhemm <159470517+epltv1@users.noreply.github.com>
2025-11-25 18:33:53 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 cd /root/obs-bot
-source venv/bin/activate
-python obs.py
+
+# Kill old
+pkill -f obs.py
+
+# Start with log
+nohup python obs.py > data/bot.log 2>&1 &
+echo "Bot started!"
